03304819453cdfde43edfe948fdc545e3f1bc9ef
William Russell <williamrussell@Williams-MacBook-Pro.local>
2023-02-06 11:44:56 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.getElementById('new-task-description');
+    handleNewTask(input.value);
+    form.reset()
+  });
 });
+
+function handleNewTask(newTask){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${newTask} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+e.target.parentNode.remove()
+}
